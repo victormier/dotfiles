8ab81d44e3521a5b054c88b71430f235597da15c
victormier <victormier@gmail.com>
2015-10-12 20:47:55 +0200

    Add brew-cask install script

---
+++ brew-cask/install.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+#
+# Brew-cask
+#
+# This installs GUI apps
+
+# Check for brew-cask
+if test ! $(brew cask --version)
+then
+  echo "  Installing brew-cask for you."
+  $(brew install caskroom/cask/brew-cask)
+fi
+
+brew update
+brew upgrade brew-cask
+brew cleanup
+brew cask cleanup
+
+# Install packages
+
+apps=(
+  google-drive
+  iterm2
+  google-chrome-canary
+  virtualbox
+  vlc
+)
+
+brew cask install "${apps[@]}"
+
+exit 0

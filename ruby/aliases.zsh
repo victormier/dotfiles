8cdabc056a917366af04512f880190de6c866ac8
victormier <victormier@gmail.com>
2017-07-10 12:34:30 +0200

    Add aliases for ruby commands in docker

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,3 +1,6 @@
 alias sc='script/console'
 alias sg='script/generate'
 alias sd='script/destroy'
+
+alias docker-rake='docker-compose run api bundle exec rake'
+alias docker-spec='docker-compose run api bundle exec rspec'

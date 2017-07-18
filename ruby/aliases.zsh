edeb02c4583172e63e69186791e3a91d4523da5b
victormier <victormier@gmail.com>
2017-07-18 17:07:34 +0200

    Update docker-compose aliases service name

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -2,5 +2,5 @@ alias sc='script/console'
 alias sg='script/generate'
 alias sd='script/destroy'
 
-alias docker-rake='docker-compose run api bundle exec rake'
-alias docker-spec='docker-compose run api bundle exec rspec'
+alias docker-rake='docker-compose run web bundle exec rake'
+alias docker-spec='docker-compose run web bundle exec rspec'

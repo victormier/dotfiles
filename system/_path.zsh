dccb11003988e406b1053506b98f79c384cb789f
victormier <victormier@gmail.com>
2018-03-12 12:30:43 +0100

    Add android aliases

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,5 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:/opt/local/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+export ANDROID_HOME=$HOME/Library/Android/sdk
+export PATH=$PATH:$ANDROID_HOME/tools
+export PATH=$PATH:$ANDROID_HOME/platform-tools
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,11 @@
 alias reload!='. ~/.zshrc'
+
+# rake
+alias rake='noglob rake'
+
+# Android dev / Sam
+alias rnra='react-native run-android'
+alias shake='adb shell input keyevent KEYCODE_MENU'
+alias samkill='adb shell am force-stop io.meetsam.android.debug'
+alias adblink='adb reverse tcp:8081 tcp:8081'
+alias gradlekill='~/Sam/sam/android/gradlew --stop'

6df4aaaae29ddc830910645ebb8fb76f2edf38ad
Victor Mier <victormier@gmail.com>
2020-10-09 08:13:33 +0200

    Add alias for updating rbenv

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -5,3 +5,5 @@ alias sd='script/destroy'
 alias docker-rake='docker-compose run web bundle exec rake'
 alias docker-racksh='docker-compose run web bundle exec racksh'
 alias docker-spec='docker-compose run test bundle exec rspec'
+
+alias rbenv-update='cd ~/.rbenv/plugins/ruby-build/ && git pull'

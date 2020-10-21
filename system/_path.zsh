305c7fad0c2caf2d6aa092264b7bd62a4c38b17d
victormier <victormier@gmail.com>
2020-10-21 13:54:46 +0200

    ZSH -> DOTFILES

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,4 +1,4 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:/opt/local/bin:$PATH"
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:/opt/local/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 export ANDROID_HOME=$HOME/Library/Android/sdk
 export PATH=$PATH:$ANDROID_HOME/tools

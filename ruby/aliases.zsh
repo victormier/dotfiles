1c9195e7d47b831c477e1b829e1b1fa2f55f8746
victormier <victormier@gmail.com>
2020-10-21 13:13:10 +0200

    Add ruby bundle exec alias

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -7,3 +7,5 @@ alias docker-racksh='docker-compose run web bundle exec racksh'
 alias docker-spec='docker-compose run test bundle exec rspec'
 
 alias rbenv-update='cd ~/.rbenv/plugins/ruby-build/ && git pull'
+
+alias bx='bundle exec'

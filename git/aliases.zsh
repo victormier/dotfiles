8d4140afaaae0742c12d7cb3eede10f312f62233
victormier <victormier@gmail.com>
2020-10-28 11:51:51 +0100

    Add git alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -18,3 +18,4 @@ alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias gfr="git push origin develop master --follow-tags"

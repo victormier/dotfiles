ffc4139eb3bbc976f7d97628912316ede3b45e97
Victor Mier <victormier@gmail.com>
2021-03-09 16:34:09 +0100

    Add new android/react native paths to $PATH

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,5 +1,7 @@
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:/opt/local/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 export ANDROID_HOME=$HOME/Library/Android/sdk
+export PATH=$PATH:$ANDROID_HOME/emulator
 export PATH=$PATH:$ANDROID_HOME/tools
+export PATH=$PATH:$ANDROID_HOME/tools/bin
 export PATH=$PATH:$ANDROID_HOME/platform-tools

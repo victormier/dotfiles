7358842ae14b0f7ebfe410e8263b2be5b2f7522a
victormier <victormier@gmail.com>
2024-10-15 09:53:27 +0200

    Add ngrok completion

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,3 +3,8 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+# ngrok
+if command -v ngrok &>/dev/null; then
+  eval "$(ngrok completion)"
+fi
